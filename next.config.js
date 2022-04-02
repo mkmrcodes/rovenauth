22e9c83229ffde2803eb5b466fb081218ba21231
mkmrcodes <mkmrcodes@gmail.com>
2022-04-02 16:25:46 +0300

    facebook image url2 added

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
       'avatars.githubusercontent.com',
       'lh3.googleusercontent.com',
       'scontent-ort2-1.xx.fbcdn.net',
+      'scontent-iad3-1.xx.fbcdn.net',
     ],
   },
 };
